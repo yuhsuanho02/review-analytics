643c6941a30a5b1a18ac566efd26bd76cb766487
Adam Ho <yuhsuanho02@gmail.com>
2022-01-11 21:37:51 +0800

    basic version

--- review-analytics.py
+++ review-analytics.py
@@ -3,10 +3,16 @@ count = 0
 with open('reviews.txt', 'r') as rvw:
     for cmt in rvw:
         commont.append(cmt)
-        count += 1
-        if count % 10000 == 0:
-            print(len(commont))
-print(len(commont))
-print(commont[0])
-print('-------------------------------')
-print(commont[1])
+        #count += 1
+        #if count % 10000 == 0:
+            #print(len(commont))
+print('檔案讀取完了, 總共有', len(commont), '筆資料')
+sum_len = 0
+for d in commont:
+    sum_len = sum_len+len(d)
+print(sum_len)
+print('留言的平均長度為', sum_len/len(commont))
+
+
+
+
